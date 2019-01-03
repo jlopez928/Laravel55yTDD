f0a0fb35a7886b9e7b8a7fde059759881722fb96
jesus lopez <jesus.lopez@venalum.com.ve>
2019-01-03 11:47:32 -0400

    5.-Creacion y Uso de Controladores

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    //
+    public function index() {
+        return 'Usuarios';
+    }
+
+    //
+    public function show($id) {
+        //return 'Mostrando detalle del usuario: ' .$id;
+        return "Mostrando detalle del usuario: {$id}";
+    }
+
+    //
+    public function create() {
+        return "Crear usuario nuevo";
+    }
+
+    //
+    public function detalle() {
+        return 'Mostrando detalle del usuario: ' .$_GET['id'];
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -19,30 +19,13 @@ Route::get('/', function () {
     return 'Hola Mundo!!';
 });
 
-Route::get('/usuarios', function () {
-    return 'Usuarios';
-});
+Route::get('/usuarios', 'UserController@index');
 
-Route::get('/usuarios/detalles', function () {
-    return 'Mostrando detalle del usuario: ' .$_GET['id'];
-});
+Route::get('/usuarios/detalles', 'UserController@detalle');
 
-Route::get('/usuarios/{id}', function ($id) {
-    //return 'Mostrando detalle del usuario: ' .$id;
-    return "Mostrando detalle del usuario: {$id}";
-})->where('id','[0-9]+'); // Expresion regular para que solo acepte numeros como id
+Route::get('/usuarios/{id}', 'UserController@show')->where('id','[0-9]+'); // Expresion regular para que solo acepte numeros como id
 
-Route::get('/usuarios/nuevo', function () {
-    return "Crear usuario nuevo";
-});
+Route::get('/usuarios/nuevo', 'UserController@create');
 
 // El ? en apodo significa que es opcional
-Route::get('/saludo/{name}/{nickname?}', function ($name,$nickname = null) {
-    $name = ucfirst($name);
-
-    if ($nickname){
-        return "Bienvenido {$name}, tu apodo es {$nickname}";
-    }else {
-        return "Bienvenido {$name}";
-    }
-});
+Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController@index');
