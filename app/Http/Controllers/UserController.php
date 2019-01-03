88c9e29ccf8bce8e5f57387229f7a2a1646d2b0b
jesus lopez <jesus.lopez@venalum.com.ve>
2019-01-03 13:23:07 -0400

    6.-Creacion y Uso Vistas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,40 @@ class UserController extends Controller
 
     //
     public function index() {
-        return 'Usuarios';
+        $users = [
+            'Joel',
+            'Ellie',
+            'Tess',
+            'Tommy',
+            'Bill',
+            '<script>alert("Clicker")</script>'
+        ];
+
+        $title = 'Listado de Usuarios';
+        
+        //Metodo Array Asociativo
+        /*return view('users', [
+            'users' => $users,
+            'title' => 'Lista de Usuarios'
+        ]);*/
+
+        //Comprobar el resultado de una funcion
+        /*dd(compact('users','title'));*/
+
+        //Otra forma - Metodo Array Asociativo
+        return view('users', compact('users','title'));
+
+        //Metodo With
+        /*return view('users')->with([
+            'users' => $users,
+            'title' => 'Lista de Usuarios'
+        ]);*/
+        
+        //Otra forma - Metodo With
+/*        return view('users')
+                ->with('users',$users)
+                ->with('title','Lista de Usuarios');*/
+
     }
 
     //
@@ -17,7 +50,7 @@ class UserController extends Controller
         //return 'Mostrando detalle del usuario: ' .$id;
         return "Mostrando detalle del usuario: {$id}";
     }
-
+    
     //
     public function create() {
         return "Crear usuario nuevo";
--- tests/Feature/UserModuleTest.php
+++ tests/Feature/UserModuleTest.php
@@ -14,7 +14,9 @@ class UserModuleTest extends TestCase
     {
         $this->get('/usuarios')
         ->assertStatus(200)
-        ->assertSee('Usuarios');
+        ->assertSee('Usuarios')
+        ->assertSee('Joel')
+        ->assertSee('Ellie');
     }
 
     /** @test */
