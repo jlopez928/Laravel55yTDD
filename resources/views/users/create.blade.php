1d94795082159680a28e4e6d3121b4ef42417d04
jesus lopez <jesus.lopez@venalum.com.ve>
2019-01-23 13:59:52 -0400

    30.-Mostrar mensajes de errores de validación

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,17 +6,32 @@
 
     <h1>Crear Usuario</h1>
 
+        @if($errors->any())
+            <div class="alert alert-danger">
+                {{--  <p>Hay Errores!!</p> --}}
+                <p>Por favor corrige los errores debajo:</p>
+                 {{--  <ul>
+                    @foreach ( $errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>   --}}
+            </div>
+        @endif
+    
     {{--  <form action="{{ url('usuarios/crear') }}" method="post">--}}
     <form action="{{ url('usuarios') }}" method="post">
 
           {{  csrf_field() }}
 
           <label for="name">Nombre:</label> <br>
-          <input type="text" name="name" id="name" placeholder="Pedro Perez">
+          <input type="text" name="name" id="name" placeholder="Pedro Perez" value="{{ old('name') }}">
+          @if($errors->has('name'))
+            <p>{{ $errors->first('name') }}</p>
+          @endif
           <br><br>
 
           <label for="email">Correo Electronico:</label> <br>
-          <input type="email" name="email" id="email" placeholder="pedro@example.com">
+          <input type="email" name="email" id="email" placeholder="pedro@example.com" value="{{ old('email') }}">
           <br><br>
 
           <label for="password">Contraseña:</label><br>
